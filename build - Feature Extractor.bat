55c01b7a115f76e2434503f76cdd144e08fe18eb
John Stranzl <13703354+JohnStranzl@users.noreply.github.com>
2023-05-07 23:24:04 -0400

    Add files via upload

--- build - Feature Extractor.bat
+++ build - Feature Extractor.bat
@@ -6,11 +6,11 @@ pyinstaller main.py --hidden-import=hook-sklearnneighborstypedef.py --noconfirm
 REM pyinstaller setup.py --hidden-import=hook-sklearnneighborstypedef.py --noconfirm
 
 REM Copy the splash screen graphic to the distribution folder
-copy "SplashScreen Images\Splash_007DM.jpg" dist\main
+copy "SplashScreen Images\Splash_007FE.jpg" dist\main
 
 REM Change the directory to the distribution folder and then rename the executable
 cd dist\main
-ren main.exe GRIMe-AI(DM).exe
+ren main.exe GRIMe-AI(FE).exe
 
 REM RUN THE PROGRAM TO ENSURE IT EXECUTES
 GRIMe-AI_DownloadManager
--- build.bat
+++ build.bat
@@ -1,10 +1,14 @@
 echo on
 
-pyi-makespec --path="C:\Users\Astrid Haugen\PycharmProjects\neonAI\venv\Lib\site-packages" --path="C:\Users\Astrid Haugen\PycharmProjects\neonAI" main.py
+REM pyi-makespec --path="C:\Users\Astrid Haugen\PycharmProjects\neonAI\venv\Lib\site-packages" --path="C:\Users\Astrid Haugen\PycharmProjects\neonAI" main.py
+REM "C:\Users\Astrid Haugen\AppData\Local\Programs\Python\Python311\Scripts\pyi-makespec" --path="C:\Users\Astrid Haugen\PycharmProjects\neonAI\venv\Lib\site-packages" --path="C:\Users\Astrid Haugen\PycharmProjects\neonAI" main.py
 
-pyinstaller main.py --hidden-import=hook-sklearnneighborstypedef.py --noconfirm
+REM pyinstaller -F --debug=all --noupx --noconfirm --hidden-import=hook-sklearnneighborstypedef.py main.py
+REM pyinstaller --onefile --noconfirm --hidden-import=hook-sklearnneighborstypedef.py --log-level ERROR main.py
+REM "C:\Users\Astrid Haugen\AppData\Local\Programs\Python\Python311\Scripts\pyinstaller" --onefile main.py --hidden-import=hook-sklearnneighborstypedef.py --noconfirm
 REM pyinstaller setup.py --hidden-import=hook-sklearnneighborstypedef.py --noconfirm
 
+pyinstaller --clean main.py
 REM Copy the splash screen graphic to the distribution folder
 copy "SplashScreen Images\Splash_007.jpg" dist\main
 
