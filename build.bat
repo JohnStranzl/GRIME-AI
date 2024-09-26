e7b98ff96b6e827cf95d2e79c5ab54ad81425a9d
John E Stranzl Jr <13703354+JohnStranzl@users.noreply.github.com>
2024-09-26 15:10:17 -0400

    Add files via upload

--- build.bat
+++ build.bat
@@ -8,16 +8,25 @@ REM pyinstaller --onefile --noconfirm --hidden-import=hook-sklearnneighborstyped
 REM "C:\Users\Astrid Haugen\AppData\Local\Programs\Python\Python311\Scripts\pyinstaller" --onefile main.py --hidden-import=hook-sklearnneighborstypedef.py --noconfirm
 REM pyinstaller setup.py --hidden-import=hook-sklearnneighborstypedef.py --noconfirm
 
+REM pyinstaller --clean --noconfirm --onedir --hidden-import playsound --contents-directory "." --add-data ".\venv\Lib\site-packages\ultralytics\cfg\default.yaml; .\ultralytics\cfg" main.py
+
 pyinstaller --clean --noconfirm --onedir --hidden-import playsound --contents-directory "." main.py
+
+
 REM Copy the splash screen graphic to the distribution folder
 copy "SplashScreen Images\Splash_007.jpg" dist\main
+copy "SplashScreen Images\GRIME-AI Logo.jpg" dist\main
+copy "SplashScreen Images\GRIME-AI Logo.jpg" dist\main
+mkdir "dist\main\ultralytics"
+mkdir "dist\main\ultralytics\cfg"
+copy "venv\Lib\site-packages\ultralytics\cfg\default.yaml" "dist\main\ultralytics\cfg"
 
 REM Change the directory to the distribution folder and then rename the executable
 cd dist\main
-ren main.exe GRIMe-AI.exe
+ren main.exe GRIME-AI.exe
 
 REM RUN THE PROGRAM TO ENSURE IT EXECUTES
-GRIMe-AI
+GRIME-AI
 
 REM cd ..\..\
 
