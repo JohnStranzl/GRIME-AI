6e1297b9600008cebe42b0be2041ec86c4f30079
John E Stranzl Jr <13703354+JohnStranzl@users.noreply.github.com>
2025-04-04 23:32:33 -0400

    Add files via upload
    Release notes, package requirements, and main ui converted to python.
    
    Signed-off-by: John E Stranzl Jr <13703354+JohnStranzl@users.noreply.github.com>

--- build.bat
+++ build.bat
@@ -10,24 +10,47 @@ REM pyinstaller setup.py --hidden-import=hook-sklearnneighborstypedef.py --nocon
 
 REM pyinstaller --clean --noconfirm --onedir --hidden-import playsound --contents-directory "." --add-data ".\venv\Lib\site-packages\ultralytics\cfg\default.yaml; .\ultralytics\cfg" main.py
 
-pyinstaller --clean --noconfirm --onedir --hidden-import playsound --contents-directory "." main.py
 
 
+REM Generate the .spec file with hidden imports
+REM pyi-makespec --onefile --noconfirm --hidden-import=playsound --hidden-import=imageio --hidden-import=imageio_ffmpeg main.py
+
+REM pyinstaller --clean --noconfirm main.spec
+@echo on
+
+REM Build the executable with PyInstaller
+pyinstaller --clean --noconfirm --onedir ^
+--hidden-import=playsound ^
+--hidden-import=imageio ^
+--hidden-import=imageio_ffmpeg ^
+--hidden-import=openpyxl ^
+--hidden-import=scikit-image ^
+--contents-directory "." main.py
+
+REM Ensure the distribution folder exists
+if not exist "dist\main" mkdir "dist\main"
+
+REM
 REM Copy the splash screen graphic to the distribution folder
 copy "SplashScreen Images\Splash_007.jpg" dist\main
 copy "SplashScreen Images\GRIME-AI Logo.jpg" dist\main
 copy "SplashScreen Images\GRIME-AI Logo.jpg" dist\main
-mkdir "dist\main\ultralytics"
-mkdir "dist\main\ultralytics\cfg"
-copy "venv\Lib\site-packages\ultralytics\cfg\default.yaml" "dist\main\ultralytics\cfg"
+
+REM Copy additional required directories and files
+if not exist "dist\main\sam2" mkdir "dist\main\sam2"
+xcopy "venv\Lib\site-packages\sam2" "dist\main\sam2" /s /e || echo Error copying sam2 folder
+
+if not exist "dist\main\ultralytics" mkdir "dist\main\ultralytics"
+if not exist "dist\main\ultralytics\cfg" mkdir "dist\main\ultralytics\cfg"
+copy "venv\Lib\site-packages\ultralytics\cfg\default.yaml" "dist\main\ultralytics\cfg" || echo Error copying ultralytics configuration
 
 REM Change the directory to the distribution folder and then rename the executable
 cd dist\main
-ren main.exe GRIME-AI.exe
+ren main.exe GRIME-AI.exe || echo Error renaming executable
 
-REM RUN THE PROGRAM TO ENSURE IT EXECUTES
-GRIME-AI
+REM Run the executable to validate its functionality
+GRIME-AI || echo Error running the program
 
-REM cd ..\..\
-
-echo off
+REM Return to the root directory
+cd ..\..\
+@echo off
