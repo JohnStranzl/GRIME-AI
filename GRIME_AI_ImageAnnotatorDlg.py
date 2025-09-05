6d051b068b36431fdb67af7155fee66a8beb32b3
John E Stranzl Jr <13703354+JohnStranzl@users.noreply.github.com>
2025-09-05 17:49:42 -0400

    Updated headers.

--- GRIME_AI_ImageAnnotatorDlg.py
+++ GRIME_AI_ImageAnnotatorDlg.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Author: John Edward Stranzl, Jr.
+# Affiliation(s): University of Nebraska-Lincoln, Blade Vision Systems, LLC
+# Contact: jstranzl2@huskers.unl.edu, johnstranzl@gmail.com
+# Created: Mar 6, 2022
+# License: Apache License, Version 2.0, http://www.apache.org/licenses/LICENSE-2.0
+
 from typing import List, Dict
 
 from PyQt5 import QtCore, QtWidgets
